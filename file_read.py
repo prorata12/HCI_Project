e530b40103d4c767f5750abb328d7561864141c5
prorata12 <prorata@kaist.ac.kr>
2020-11-27 04:20:50 +0900

    Update file_read.py

--- file_read.py
+++ file_read.py
@@ -11,17 +11,19 @@ f2_label = 1
 
 # File Path generator (Folder Path + File Name)
 import copy
-def file_path_generator(path, _filelist):
-    filelist = copy.deepcopy(_filelist)
+def file_path_generator(path):
+    # path : folder path that contains pictures (same emtions, same train/test)
+    # return value : filelist(list type) that contain full paths of pictures
+    filelist = os.listdir(path)
     for idx,filename in enumerate(filelist):
         filelist[idx] = path + '/' + filename
     return filelist
 
 # These contain File Path
-f1_train_list = file_path_generator(f1_train_path, os.listdir(f1_train_path))
-f1_test_list = file_path_generator(f1_test_path, os.listdir(f1_test_path))
-f2_train_list = file_path_generator(f2_train_path, os.listdir(f2_train_path))
-f2_test_list = file_path_generator(f2_test_path, os.listdir(f2_test_path))
+f1_train_list = file_path_generator(f1_train_path)
+f1_test_list = file_path_generator(f1_test_path)
+f2_train_list = file_path_generator(f2_train_path)
+f2_test_list = file_path_generator(f2_test_path)
 
 # Return value: list of [train list, test list, label]
 file_list = [[f1_train_list, f1_test_list, f1_label],\
