f6195b79f6fb259592435c474c056ae7d36ee200
prorata12 <prorata@kaist.ac.kr>
2020-11-29 16:44:20 +0900

    File Reading Way Change
    Now Path should be "train/emotion/picture.jpg" format

--- file_read.py
+++ file_read.py
@@ -1,14 +1,5 @@
 import os
 
-# Folder Path
-f1_train_path = "D:/Desktop_D/HCI_Project/archive/train/happy"
-f1_test_path = "D:/Desktop_D/HCI_Project/archive/test/happy"
-f1_label = 0
-
-f2_train_path = "D:/Desktop_D/HCI_Project/archive/train/angry"
-f2_test_path = "D:/Desktop_D/HCI_Project/archive/test/angry"
-f2_label = 1
-
 # File Path generator (Folder Path + File Name)
 import copy
 def file_path_generator(path):
@@ -19,13 +10,24 @@ def file_path_generator(path):
         filelist[idx] = path + '/' + filename
     return filelist
 
+# Picture paths for each emotion
+train_paths = file_path_generator("D:/Desktop_D/HCI_Project/archive/train")
+test_paths = file_path_generator("D:/Desktop_D/HCI_Project/archive/test")
+# Folder path example : archive/train/angry/pic1.jpg
+# Folder path example : archive/train/angry/pic2.jpg
+# Folder path example : archive/train/sad/pic1.jpg
+# Folder path example : archive/test/angry/pic1.jpg
+
 # These contain File Path
-f1_train_list = file_path_generator(f1_train_path)
-f1_test_list = file_path_generator(f1_test_path)
-f2_train_list = file_path_generator(f2_train_path)
-f2_test_list = file_path_generator(f2_test_path)
+train_pictures = []
+test_pictures = []
 
 
-# Return value: list of [train list, test list, label]
-file_list = [[f1_train_list, f1_test_list, f1_label],\
-    [f2_train_list, f2_test_list, f2_label]]
+for folder_path in train_paths:
+    train_pictures.append(file_path_generator(folder_path))
+# train_pictures[0][3] : it stores the path of third image in 0 th emotion, 
+
+for folder_path in test_paths:
+    test_pictures.append(file_path_generator(folder_path))
+
+# Return value : train_pictures, test_pictures
